969fefa81f3ecdd69d8b434c811a071eacf8afe2
anasm20 <agrimohamad@gmail.com>
2023-01-13 00:32:34 +0100

    logo

---
+++ Projekt/Seiten/logik.js
@@ -0,0 +1,13 @@
+document.getElementById("SAMSUNG").style.display = "none";
+
+
+function produktAnzeigen(nr) {
+    if (nr === 1) {
+        document.getElementById("IPHONE").style.display = "block";
+        document.getElementById("SAMSUNG").style.display = "none";
+    }
+    if (nr === 2) {
+        document.getElementById("IPHONE").style.display = "block";
+        document.getElementById("SAMSUNG").style.display = "none";
+    }
+}
