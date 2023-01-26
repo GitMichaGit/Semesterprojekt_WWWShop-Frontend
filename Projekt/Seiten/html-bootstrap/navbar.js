1a348c5eea63899da142cbc38719693c2b7228b1
GitMichaGit <mbeyer98@yahoo.com>
2023-01-26 22:56:30 +0100

    Seiten bearbeitet, Impressum fertiggestellt

--- Projekt/Seiten/html-bootstrap/navbar.js
+++ Projekt/Seiten/html-bootstrap/navbar.js
@@ -1,22 +1,17 @@
 fetch('nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#replace_with_navbar");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
 
 fetch('nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_footer");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
-
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#replace_with_footer");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
